b5236ae0ff72231334e65ad089f03cc98d93d8b7
src8655 <src8655@naver.com>
2019-05-01 16:46:06 +0900

    채팅완성

--- src/main/java/com/cafe24/network/chat/Chat.java
+++ src/main/java/com/cafe24/network/chat/Chat.java
@@ -7,7 +7,7 @@ import com.cafe24.network.chat.windows.ChatWindows;
 public class Chat extends JFrame{
 
 	public static void main(String[] args) {
-		ChatWindows chatWindows = new ChatWindows();
+		ChatWindows chatWindows = new ChatWindows();	//채팅시작
 	}
 
 }
